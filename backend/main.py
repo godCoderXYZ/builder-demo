224168479d9b147a2170e1c00cfabb62f2b23617
Micah <smicah169@gmail.com>
2025-11-23 22:33:06 +0800

    Changed Port

--- backend/main.py
+++ backend/main.py
@@ -336,7 +336,7 @@ async def predict(predict: Predict):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     print(f"🚀 Starting server on port {port}")
     uvicorn.run(app, host="0.0.0.0", port=port)
 
