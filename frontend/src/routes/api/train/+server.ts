67fdd3cbfffc02cc27926ef03fd48f65c766ce33
Micah <smicah169@gmail.com>
2025-11-24 17:21:21 +0800

    Added Debugging on /api/train route

--- frontend/src/routes/api/train/+server.ts
+++ frontend/src/routes/api/train/+server.ts
@@ -3,6 +3,9 @@ import type { RequestHandler } from './$types';
 export const POST: RequestHandler = (async ({request}) => {
     const inputs = await request.json();
 
+    console.log("Starting training process (/api/train/server.ts route)...");
+    console.log("Inputs received for training:", inputs);
+
     // const response = await fetch('http://127.0.0.1:8080/train/', {
     // const response = await fetch('https://builder-demo-production-f269.up.railway.app/train/', {
     const response = await fetch('https://deep-dive-into-ai-backend.onrender.com/train/', {
@@ -13,9 +16,13 @@ export const POST: RequestHandler = (async ({request}) => {
         body: JSON.stringify( inputs ),
     });
 
+    console.log("Training process completed. Processing response...");
+
     if (!response.ok) {
         throw new Error(`Error: ${await response.text()}`)
     }
+
+    console.log("Response OK. Parsing JSON...");
 
     const responseJSON = await response.json();
     
